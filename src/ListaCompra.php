759e4da176ba9d93a2643982db6bd31bb00971a6
Joritz <echenique.149324@e.unavarra.es>
2025-04-01 13:11:22 +0200

    [refactor] - Utilizacion_array_en_vez_de_string_y_ajuste_para_su_implementacion

--- src/ListaCompra.php
+++ src/ListaCompra.php
@@ -4,21 +4,47 @@ namespace Deg540\DockerPHPBoilerplate;
 
 class ListaCompra
 {
-    public string $listaCompra =  '';
+    public array $listaCompra =  [];
     public function add(string $string):string
     {
+        $string = explode(' ', $string);
+        if($string[0] != ''){
 
-        if($string != ''){
+            if(array_key_exists($string[0], $this->listaCompra)){
+                $this->listaCompra[$string[0]] = intval($this->listaCompra[$string[0]]) + intval($string[1]);
+            }
 
-            if(strpos($string, ' ')){
-                $string = explode(' ', $string);
-                return $this->listaCompra . $string[0] . ' x'. $string[1];
+            else{
+
+                if(sizeof($string) > 1){
+                    $this->listaCompra[$string[0]] = $string[1];
+                }
+
+                else{
+                    $this->listaCompra[$string[0]] = 1;
+                }
 
             }
-            $this->listaCompra = $string;
-            return $this->listaCompra . ' x1';
 
         }
-        return $string;
+        return $this->imprimirLista();
+    }
+
+    private function imprimirLista():string
+    {
+        return $this->mapped_implode(', ', $this->listaCompra, ' x');
+
+    }
+
+    function mapped_implode($glue, $array, $symbol = '=') {
+
+        return implode($glue, array_map(
+                function($k, $v) use($symbol) {
+                    return $k . $symbol . $v;
+                },
+                array_keys($array),
+                array_values($array)
+            )
+        );
     }
 }
